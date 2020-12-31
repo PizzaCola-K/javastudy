e099a81ad1d8320a64387a88a11346c659ff8602
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 14:18:29 +0900

    feat: find a pair of dice that sum of pair is 6

---
+++ jungsuk_basic/ch04/Exercise4_6.java
@@ -0,0 +1,22 @@
+package ch04;
+
+public class Exercise4_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int dice1 = 1; dice1 <= 6 ; dice1++) {
+			for (int dice2 = 1; dice2 <= 6; dice2++) {
+				if(dice1+dice2 == 6) {
+					System.out.printf("(%d, %d)%n", dice1,dice2);
+				}
+			}
+		}
+		System.out.println();
+		for(int dice1 = 1; dice1 <= 6 ; dice1++) {
+			int dice2 = 6-dice1;
+			if(1<=dice2&&dice2 <= 6)
+				System.out.printf("(%d, %d)%n", dice1,dice2);
+		}
+	}
+
+}
