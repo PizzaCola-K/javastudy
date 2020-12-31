ab7b7e662f75e67411ce0af6d17249e5db0db70f
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 11:35:53 +0900

    feat: Ex4_15 Number Match Game using do-while

---
+++ jungsuk_basic/ch04/Ex4_15.java
@@ -0,0 +1,27 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class Ex4_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input = 0, answer = 0;
+		
+		answer = (int)(Math.random()*100)+1;
+		Scanner scanner = new Scanner(System.in);
+		
+		do {
+			System.out.print("1과 100 사이의 정수를 입력하세요.>");
+			input = scanner.nextInt();
+			if (answer < input) {
+				System.out.println("더 작은 값으로 다시 시도하세요.");
+			} else if(answer > input) {
+				System.out.println("더 큰 값으로 다시 시도하세요.");
+			}
+		} while(input != answer);
+		System.out.println("정답입니다.");
+		scanner.close();
+	}
+
+}
