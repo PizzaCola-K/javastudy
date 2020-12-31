59800b922512a4a6992a58cdfa2a50c1c4bd5305
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 20:18:05 +0900

    feat: Exercise6_5 PlayingCard

---
+++ jungsuk_basic/ch06/PlayingCard.java
@@ -0,0 +1,20 @@
+package ch06;
+
+public class PlayingCard {
+	int kind; // instance variable
+	int num;  // instance variable
+	
+	static int width;  // static variable
+	static int height; // static variable
+	
+	PlayingCard(int k, int n){
+		kind = k;
+		num = n;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PlayingCard card = new PlayingCard(1, 1); // local variable
+	}
+
+}
