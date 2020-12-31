3364f40e7bfb6b5b38c8a533366376c68a8ad33c
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-01 08:26:33 +0900

    feat: Ex7_1 My first inherit

---
+++ jungsuk_basic/ch07/Ex7_1.java
@@ -0,0 +1,34 @@
+package ch07;
+
+public class Ex7_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SmartTv sTv = new SmartTv();
+		sTv.channel = 10;
+		sTv.channelUp();
+		System.out.println(sTv.channel);
+		sTv.displayCaption("Hello World!");
+		sTv.caption =true;
+		sTv.displayCaption("Hello World!");
+		
+	}
+
+}
+
+class Tv {
+	boolean power;
+	int channel;
+	void power() {power = !power;}
+	void channelUp() {++channel;}
+	void channelDown() {--channel;}
+}
+
+class SmartTv extends Tv {
+	boolean caption;
+	void displayCaption(String text) {
+		if(caption) {
+			System.out.println(text);
+		}
+	}
+}
