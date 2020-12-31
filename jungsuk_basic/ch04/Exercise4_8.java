65ffaa2ddb08a2e87af5691cca8c765e5d43854e
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 14:27:18 +0900

    feat: Exercise4_8 random int number generator

---
+++ jungsuk_basic/ch04/Exercise4_8.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class Exercise4_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = (int)(Math.random()*6)+1;
+		System.out.println("value:"+value);
+	}
+
+}
