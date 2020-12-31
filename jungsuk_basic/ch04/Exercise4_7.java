6042565de1db884e92819252a67076ee94675bf4
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 14:24:05 +0900

    feat: Exercise4_7 sum of string digit

---
+++ jungsuk_basic/ch04/Exercise4_7.java
@@ -0,0 +1,16 @@
+package ch04;
+
+public class Exercise4_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "12345";
+		int sum = 0;
+		for (int i = 0; i < str.length(); i++) {
+			sum += str.charAt(i)-'0';
+			System.out.println("+"+str.charAt(i));
+		}
+		System.out.println("="+sum);
+	}
+
+}
