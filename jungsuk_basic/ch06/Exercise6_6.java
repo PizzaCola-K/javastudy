19bcffd7a42f7678b95857f08817cb07abaa7053
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 20:21:32 +0900

    feat: Exercise6_6 MyPoint class with getDistance Method

---
+++ jungsuk_basic/ch06/Exercise6_6.java
@@ -0,0 +1,26 @@
+package ch06;
+
+public class Exercise6_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyPoint p = new MyPoint(1, 1);
+		
+		System.out.println(p.getDistance(2, 2));
+	}
+
+}
+
+class MyPoint {
+	int x;
+	int y;
+	
+	MyPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	double getDistance(int x, int y) {
+		return Math.sqrt((x-this.x)*(x-this.x)+(y-this.y)*(y-this.y));
+	}
+}
