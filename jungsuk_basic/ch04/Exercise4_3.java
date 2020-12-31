59bf9167664f866fa5c8739f5ca9a343135600d9
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 13:00:52 +0900

    feat: Exercise4_3 sum of 1+(1+2)+...(1+2+...+10)

---
+++ jungsuk_basic/ch04/Exercise4_3.java
@@ -0,0 +1,18 @@
+package ch04;
+
+public class Exercise4_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i = 1; i <= 10; i++) {
+			for(int j = 1; j <= i; j++) {
+				sum +=j;
+				System.out.print("+"+j);
+			}
+			System.out.println();
+		}
+		System.out.println(sum);
+	}
+
+}
