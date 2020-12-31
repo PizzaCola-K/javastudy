aa703dce7929076b12091181dab84de2946b61e8
Kihyuk Sung <SamuelSung.dev@gmail.com>
2020-12-31 16:45:43 +0900

    feat: Exercise5_1 Array Declaration

---
+++ jungsuk_basic/ch05/Exercise5_1.java
@@ -0,0 +1,18 @@
+package ch05;
+import java.util.Arrays;
+
+public class Exercise5_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr[];
+		int[] arr1 = {1,2,3,};
+		int[] arr2 = new int[5];
+		//int[] arr3 = new int[5]{1,2,3,4,5}; 두번째 대괄호 숫자 x
+		//int arr4[5]; 배열 선언시 크기 지정 못함.
+		int[] arr5[] = new int[3][]; 
+		
+		System.out.println(Arrays.toString(arr1));
+	}
+
+}
