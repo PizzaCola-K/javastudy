dddf687c2dac30724e6ea974ccdaad58a0f9ee35
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-01 14:42:30 +0900

    feat: Ex8_3 Ex8_4 Error and Catch block

---
+++ jungsuk_basic/ch08/Ex8_3.java
@@ -0,0 +1,21 @@
+package ch08;
+
+class Ex8_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(2);
+		//System.out.println(0/0);
+		try {
+			System.out.println(3);
+			System.out.println(0/0);
+			System.out.println(4);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(5);
+		}
+		System.out.println(6);
+	}
+
+}
---
+++ jungsuk_basic/ch08/Ex8_4.java
@@ -0,0 +1,26 @@
+package ch08;
+
+class Ex8_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(0/0);
+			System.out.println(4);
+		} catch (ArithmeticException ae) {
+			// TODO: handle exception
+			if(ae instanceof ArithmeticException) {
+				System.out.println(true);
+			}
+			System.out.println("ArithmeticException");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Exception");
+		}
+		System.out.println(6);
+	}
+
+}
