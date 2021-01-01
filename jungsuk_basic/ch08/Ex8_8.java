8bcf205f484b45dc9353cbb0ce3d92526d9e8656
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-01 14:55:41 +0900

    feat: Ex8_7 Ex8_8 checked / unchecked Exception

---
+++ jungsuk_basic/ch08/Ex8_8.java
@@ -0,0 +1,11 @@
+package ch08;
+
+class Ex8_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//throw new Exception();
+		throw new RuntimeException();
+	}
+
+}
