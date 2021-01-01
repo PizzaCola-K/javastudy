c6e1cbf8fff290dd5e377aa5cfdb0680318557ad
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-01 17:15:04 +0900

    feat: Ex9_7 Ex9_8 String literal and empty String

---
+++ jungsuk_basic/ch09/Ex9_7.java
@@ -0,0 +1,13 @@
+package ch09;
+
+class Ex9_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "AAA";
+		String s2 = "AAA";
+		String s3 = "AAA";
+		String s4 = "BBB";
+	}
+
+}
