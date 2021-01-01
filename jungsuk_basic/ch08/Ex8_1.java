c254f929584a123f2fe517329814d310648f680e
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-01 14:37:44 +0900

    feat: Ex8_1 Ex8_2 try-catch flow

---
+++ jungsuk_basic/ch08/Ex8_1.java
@@ -0,0 +1,18 @@
+package ch08;
+
+class Ex8_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		try {
+			System.out.println(2);
+			System.out.println(3);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(4);
+		}
+		System.out.println(5);
+	}
+
+}
