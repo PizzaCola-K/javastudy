7dbbf0ff905c9fd74d427de2ab57305e58db276c
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-05 17:32:13 +0900

    feat: Ex12_4 generic wildcard

--- jungsuk_basic/ch12/Box.java
+++ jungsuk_basic/ch12/Box.java
@@ -6,6 +6,7 @@ class Box<T> {
     ArrayList<T> list = new ArrayList<T>();
     void add(T item) {list.add(item);}
     T get(int i) {return list.get(i);}
+    ArrayList<T> getList() {return list;}
     int size() {return list.size();}
     public String toString() {return list.toString();}
 }
--- jungsuk_basic/ch12/Fruit.java
+++ jungsuk_basic/ch12/Fruit.java
@@ -10,3 +10,20 @@ class Grape extends Fruit { public String toString() {return "Grape";}}
 class Toy                 { public String toString() {return "Toy";}}
 
 interface Eatable{}
+
+class Juice {
+    String name;
+    Juice(String name) {this.name = name+"Juice";}
+    public String toString() {return name;}
+}
+
+class Juicer {
+    static Juice makeJuice(FruitBox<? extends Fruit> box) {
+        String tmp = "";
+
+        for(Fruit f : box.getList()) {
+            tmp += f+" ";
+        }
+        return new Juice(tmp);
+    }
+}
