eb28a2ac2cfe8addada0130035995e355b0f4d3d
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-05 17:03:21 +0900

    feat: Ex12_2 Iterator<E>

---
+++ jungsuk_basic/ch12/Student.java
@@ -0,0 +1,13 @@
+package ch12;
+
+class Student {
+    String name = "";
+    int ban;
+    int no;
+
+    Student(String name,int ban, int no) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+    }
+}
