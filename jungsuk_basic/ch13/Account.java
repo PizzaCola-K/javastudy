ae803cc8b9057c68ba4caf0bc619770a537cbcb5
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-06 20:33:47 +0900

    feat: Ex13_12 synchronized example 1

---
+++ jungsuk_basic/ch13/Account.java
@@ -0,0 +1,16 @@
+package ch13;
+
+class Account {
+    private int balance = 1000;
+    public int getBalance() {
+        return balance;
+    }
+    public void withdraw(int money) {
+        if(balance >= money) {
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e) {}
+            balance -= money;
+        }
+    }
+}
