01904c4dea2819b6d5a4bb613ac06efbb2f3b97d
Kihyuk Sung <SamuelSung.dev@gmail.com>
2021-01-08 15:51:46 +0900

    feat: Ex14_9 reduce

---
+++ jungsuk_basic/ch14/Ex14_9.java
@@ -0,0 +1,43 @@
+package ch14;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class Ex14_9 {
+    public static void main(String[] args) {
+        String[] strArr = {
+                "Inheritance", "Java", "Lambda", "Stream",
+                "OptionalDouble", "IntStream", "count", "sum"
+        };
+
+        Stream.of(strArr).forEach(System.out::println);
+
+        boolean noEmptyStr = Stream.of(strArr).noneMatch(s->s.length()==0);
+
+        Optional<String> sWord = Stream.of(strArr).filter(s->s.charAt(0)=='s').findFirst();
+
+        System.out.println();
+        System.out.println(noEmptyStr);
+        System.out.println(sWord.get());
+
+        IntStream intStream1 = Stream.of(strArr).mapToInt(String::length);
+        IntStream intStream2 = Stream.of(strArr).mapToInt(String::length);
+        IntStream intStream3 = Stream.of(strArr).mapToInt(String::length);
+        IntStream intStream4 = Stream.of(strArr).mapToInt(String::length);
+
+        int count = intStream1.reduce(0, (a,b)->a+1);
+        int sum = intStream2.reduce(0, (a,b)->a+b);
+
+        OptionalInt max = intStream3.reduce(Integer::max);
+        OptionalInt min = intStream4.reduce(Integer::min);
+
+        System.out.println();
+        System.out.println(count);
+        System.out.println(sum);
+        System.out.println(max.getAsInt());
+        System.out.println(min.getAsInt());
+
+    }
+}
