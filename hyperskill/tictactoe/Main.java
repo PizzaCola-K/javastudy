7e6fa5aaf3b38632bbbb91851ac8bcb5aad72774
K <PizzaCola.K@gmail.com>
2021-01-13 18:22:25 +0900

    feat: Tic-Tac-Toe What's up on the field?
    Get Input from System.in and print Tic-Tac-Toe Board

--- hyperskill/tictactoe/Main.java
+++ hyperskill/tictactoe/Main.java
@@ -1,10 +1,20 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        char[] chars = input.toCharArray();
+
+        System.out.println("---------");
+        System.out.println("| " + chars[0] + " " + chars[1] + " " + chars[2] + " |");
+        System.out.println("| " + chars[3] + " " + chars[4] + " " + chars[5] + " |");
+        System.out.println("| " + chars[6] + " " + chars[7] + " " + chars[8] + " |");
+        System.out.println("---------");
     }
 }
