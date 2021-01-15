2d6e33a8debdb5db2f2c6f2d61649f9cb12012c2
K <PizzaCola.K@gmail.com>
2021-01-15 13:29:32 +0900

    feat: CoffeeMachine Ingredient calculator

--- hyperskill/coffeemachine/machine/CoffeeMachine.java
+++ hyperskill/coffeemachine/machine/CoffeeMachine.java
@@ -1,13 +1,21 @@
 package coffeemachine.machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need: ");
+
+        int cups = scanner.nextInt();
+        int water = cups * 200;
+        int milk = cups * 50;
+        int coffeeBeans = cups * 15;
+
+        System.out.printf("For %d cups of coffee you will need:%n", cups);
+        System.out.printf("%d ml of water%n", water);
+        System.out.printf("%d ml of milk%n", milk);
+        System.out.printf("%d g of coffee beans%n", coffeeBeans);
+
     }
 }
