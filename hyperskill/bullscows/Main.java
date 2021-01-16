8ab100920a79d3b6bfd726ad8445d1daa5171c3f
K <PizzaCola.K@gmail.com>
2021-01-16 12:53:33 +0900

    feat: Bulls and Cows Game log

---
+++ hyperskill/bullscows/Main.java
@@ -0,0 +1,18 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.");
+
+        System.out.println();
+        System.out.printf("Turn %d. Answer:%n", 1);
+        System.out.printf("%d%n", 1234);
+        System.out.println("Grade: 1 cow.");
+
+        System.out.println();
+        System.out.printf("Turn %d. Answer:%n", 2);
+        System.out.printf("%d%n", 9876);
+        System.out.println("Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9876.");
+    }
+}
