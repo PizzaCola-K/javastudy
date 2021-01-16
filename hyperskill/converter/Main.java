c914ec6506f0cfa90675f7dfaec52ff95537a369
K <PizzaCola.K@gmail.com>
2021-01-16 21:09:40 +0900

    feat: NumeralSystemCoverter Convert decimals

--- hyperskill/converter/Main.java
+++ hyperskill/converter/Main.java
@@ -5,10 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        if (input.matches("\\d+")) {
-            int num = Integer.parseInt(input);
-            System.out.println(num % 8);
+        String numberStr = scanner.next();
+        String radixStr = scanner.next();
+
+        long number = Long.parseLong(numberStr);
+
+        switch (radixStr) {
+            case "2":
+                System.out.println("0b" + Long.toString(number, 2));
+                break;
+            case "8":
+                System.out.println("0" + Long.toString(number, 8));
+                break;
+            case "16":
+                System.out.println("0x" + Long.toString(number, 16));
+                break;
+            default:
+
         }
     }
 }
