2894f16f02b324b6d68019e48358b99cc8af6c5b
K <PizzaCola.K@gmail.com>
2021-01-19 14:00:10 +0900

    feat: Readability Words and sentences

--- hyperskill/readability/Main.java
+++ hyperskill/readability/Main.java
@@ -1,5 +1,6 @@
 package readability;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        if (input.length() > 100) {
+        String[] sentences = input.split("[!\\.?]");
+        String[] trimedSentences = new String[sentences.length];
+
+        for (int i = 0; i < sentences.length; i++) {
+            trimedSentences[i] = sentences[i].trim();
+        }
+
+        int wordCount = Arrays.stream(trimedSentences)
+                .map(sentence -> sentence.split("\\s").length)
+                .reduce(0, Integer::sum);
+
+        if (wordCount / (double) trimedSentences.length > 10) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
